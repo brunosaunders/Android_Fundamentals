fef6c6ad9218ec1504352aef6aee43009c79151f
Bruno Saunders <senabruno97@hotmail.com>
2020-09-02 00:51:30 -0300

    3 buttons controlling boxes collors

--- ColorMyViews/app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.colormyviews
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +23,10 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+
+            R.id.button_yellow -> box_three_text.setBackgroundResource(R.color.my_yellow)
+            R.id.button_red -> box_four_text.setBackgroundResource(R.color.my_red)
+            R.id.button_black -> box_five_text.setBackgroundResource(R.color.my_black)
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
@@ -32,9 +38,12 @@ class MainActivity : AppCompatActivity() {
         val boxFour = findViewById<TextView>(R.id.box_four_text)
         val boxFive = findViewById<TextView>(R.id.box_five_text)
         val constraintView = findViewById<View>(R.id.constraint_layout)
+        val yellowButton = findViewById<Button>(R.id.button_yellow)
+        val redButton = findViewById<Button>(R.id.button_red)
+        val blackButton = findViewById<Button>(R.id.button_black)
 
         val allViews: List<View> = listOf(boxOne, boxTwo, boxThree, boxFour, boxFive,
-                                            constraintView)
+                                            constraintView, yellowButton, redButton, blackButton)
         for(item in allViews) {
             item.setOnClickListener {makeColored(it)}
         }
