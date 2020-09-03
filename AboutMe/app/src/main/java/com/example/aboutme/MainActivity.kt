d0925509e0910b9b1a37e85777adf1ff36ad8217
Bruno Saunders <senabruno97@hotmail.com>
2020-09-03 16:22:31 -0300

    making usage of databinding to display data

--- AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -11,18 +11,22 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var myName: MyName = MyName("Aleks Haecky")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        binding.myName = myName
         binding.doneButton.setOnClickListener {addNickname(it)}
         binding.nicknameText.setOnClickListener {updateNickname(it)}
     }
 
     private fun addNickname(view: View){
+
         binding.apply {
-            nicknameText.text = nicknameEdit.text.toString()
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
             nicknameEdit.visibility = View.GONE
             doneButton.visibility = View.GONE
             nicknameText.visibility = View.VISIBLE
