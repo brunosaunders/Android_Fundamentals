90a4cc4454e4f29e629481d9787d9a21243b1f0f
Bruno Saunders <senabruno97@hotmail.com>
2020-09-04 18:40:17 -0300

    incorporing fragments into navigation graph

--- AndroidTrivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ AndroidTrivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,12 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+        val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater,
+                                            R.layout.fragment_title, container, false)
+        binding.playButton.setOnClickListener { view ->
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment2)
+        }
         return binding.root
     }
 }
