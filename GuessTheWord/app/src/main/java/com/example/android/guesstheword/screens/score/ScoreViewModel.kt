031de8298da1c1ca661ee629dc458c0d178ca082
Bruno Saunders <senabruno97@hotmail.com>
2020-09-23 13:33:27 -0300

    Changing screen using LiveData observer

--- GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
+++ GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -10,8 +10,20 @@ class ScoreViewModel(finalScore: Int) : ViewModel() {
     val score: LiveData<Int>
         get() = _score
 
+    private val _eventPlayAgain = MutableLiveData<Boolean>()
+    val eventPlayAgain: LiveData<Boolean>
+        get() = _eventPlayAgain
+
     init {
         _score.value = finalScore
         Log.i("ScoreViewModel", "The final score is ${_score.value}")
+    }
+
+    fun onPlayAgain() {
+        _eventPlayAgain.value = true
+    }
+
+    fun onPlayAgainComplete() {
+        _eventPlayAgain.value = false
     }
 }
